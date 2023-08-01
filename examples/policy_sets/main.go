2a58c7c0d5fa2e6667c9548af8512adfb77042a1
Kuba Mazurkiewicz <kmazurki@cisco.com>
2023-08-01 13:49:07 +0200

    modified network_access-policy_set

--- examples/policy_sets/main.go
+++ examples/policy_sets/main.go
@@ -32,64 +32,221 @@ func main() {
 			fmt.Println("Name: ", row.Name)
 			fmt.Println("Description: ", row.Description)
 			fmt.Println("Condition: ", row.Condition)
+			if row.Condition != nil {
+				fmt.Println("Children: ", row.Condition.Children)
+			}
 		}
 	}
 	fmt.Println("-------------")
+
+	fmt.Println("executing GetNetworkAccessPolicySetsbyID...")
+	policy_set, _, err := Client.NetworkAccessPolicySet.GetNetworkAccessPolicySetByID("13727e31-93d1-4fc6-9a7e-80f6998b1916")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	if policy_set != nil && policy_set.Response != nil {
+
+		fmt.Println("-------------")
+		fmt.Println("ID: ", policy_set.Response.ID)
+		fmt.Println("Name: ", policy_set.Response.Name)
+		fmt.Println("Description: ", policy_set.Response.Description)
+		fmt.Println("Condition: ", policy_set.Response.Condition)
+		if policy_set.Response.Condition != nil {
+			fmt.Println("Children: ", policy_set.Response.Condition.Children)
+		}
+	}
+	fmt.Println("-------------")
+
 	falseVal := false
 
-	prof := &isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySet{
-		Name:        "Test_9988",
+	policy_set2 := &isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySet{
+		Name:        "Test_9983",
 		Description: "Sample policy_set using ciscoise-go-sdk",
 		ServiceName: "Default Network Access",
 		State:       "enabled",
-		IsProxy:     false
+		IsProxy:     &falseVal,
+		Condition: &isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySetCondition{
+			ConditionType: "ConditionAndBlock",
+			IsNegate:      &falseVal,
+			Children: &[]isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySetCondition{
+				{
+					ConditionType: "ConditionReference",
+					ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+				},
+				{
+					ConditionType: "ConditionReference",
+					ID:            "1e9f02d0-13e7-4a3b-8fbc-fb38330d74b5",
+				},
+				{
+					ConditionType: "ConditionAndBlock",
+					IsNegate:      &falseVal,
+					Children: &[]isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySetCondition{
+						{
+							ConditionType: "ConditionReference",
+							ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+						},
+						{
+							ConditionType: "ConditionReference",
+							ID:            "1e9f02d0-13e7-4a3b-8fbc-fb38330d74b5",
+						},
+						{
+							ConditionType: "ConditionAndBlock",
+							IsNegate:      &falseVal,
+							Children: &[]isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySetCondition{
+								{
+									ConditionType: "ConditionReference",
+									ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+								},
+								{
+									ConditionType: "ConditionReference",
+									ID:            "1e9f02d0-13e7-4a3b-8fbc-fb38330d74b5",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
 	}
 
-	reqProf := &isegosdk.RequestAuthorizationProfileCreateAuthorizationProfile{
-		AuthorizationProfile: prof,
+	policy_set1 := &isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySet{
+		Name:        "Test_9988_1",
+		Description: "Sample policy_set using ciscoise-go-sdk",
+		ServiceName: "Default Network Access",
+		State:       "enabled",
+		IsProxy:     &falseVal,
+		Condition: &isegosdk.RequestNetworkAccessPolicySetCreateNetworkAccessPolicySetCondition{
+			IsNegate:      &falseVal,
+			ConditionType: "ConditionReference",
+			ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+		},
 	}
 
-	fmt.Println("executing CreateAuthorizationProfile...")
-	_, err = Client.AuthorizationProfile.CreateAuthorizationProfile(reqProf)
+	policy_set3 := &isegosdk.RequestNetworkAccessPolicySetUpdateNetworkAccessPolicySetByID{
+		Name:        "Test_9988_1",
+		Description: "Sample policy_set using ciscoise-go-sdk",
+		ServiceName: "Default Network Access",
+		State:       "enabled",
+		IsProxy:     &falseVal,
+		Condition: &isegosdk.RequestNetworkAccessPolicySetUpdateNetworkAccessPolicySetByIDCondition{
+			IsNegate:      &falseVal,
+			ConditionType: "ConditionReference",
+			ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+		},
+	}
 
+	fmt.Println(policy_set3)
+
+	policy_set4 := &isegosdk.RequestNetworkAccessPolicySetUpdateNetworkAccessPolicySetByID{
+		Name:        "Test_9988_2",
+		Description: "Sample policy_set using ciscoise-go-sdk",
+		ServiceName: "Default Network Access",
+		State:       "enabled",
+		IsProxy:     &falseVal,
+		Condition: &isegosdk.RequestNetworkAccessPolicySetUpdateNetworkAccessPolicySetByIDCondition{
+			ConditionType: "ConditionAndBlock",
+			IsNegate:      &falseVal,
+			Children: &[]isegosdk.RequestNetworkAccessPolicySetUpdateNetworkAccessPolicySetByIDCondition{
+				{
+					ConditionType: "ConditionReference",
+					ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+				},
+				{
+					ConditionType: "ConditionReference",
+					ID:            "1e9f02d0-13e7-4a3b-8fbc-fb38330d74b5",
+				},
+				{
+					ConditionType: "ConditionAndBlock",
+					IsNegate:      &falseVal,
+					Children: &[]isegosdk.RequestNetworkAccessPolicySetUpdateNetworkAccessPolicySetByIDCondition{
+						{
+							ConditionType: "ConditionReference",
+							ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+						},
+						{
+							ConditionType: "ConditionReference",
+							ID:            "1e9f02d0-13e7-4a3b-8fbc-fb38330d74b5",
+						},
+						{
+							ConditionType: "ConditionAndBlock",
+							IsNegate:      &falseVal,
+							Children: &[]isegosdk.RequestNetworkAccessPolicySetUpdateNetworkAccessPolicySetByIDCondition{
+								{
+									ConditionType: "ConditionReference",
+									ID:            "49ec9c17-6987-43ef-9478-a42111c59b81",
+								},
+								{
+									ConditionType: "ConditionReference",
+									ID:            "1e9f02d0-13e7-4a3b-8fbc-fb38330d74b5",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	fmt.Println(policy_set1)
+	fmt.Println(policy_set2)
+	fmt.Println("executing CreatePolicySet...")
+
+	//policy_set_, _, err := Client.NetworkAccessPolicySet.CreateNetworkAccessPolicySet(policy_set2)
+	//if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//	if policy_set_ != nil {
+	//		fmt.Println(policy_set_)
+	//	}
+
+	//policy_set1_, _, err := Client.NetworkAccessPolicySet.CreateNetworkAccessPolicySet(policy_set1)
+	//if err != nil {
+	//	fmt.Println(err)
+	//}
+	//if policy_set1_ != nil {
+	//		fmt.Println(policy_set1_.Response.Condition)
+	//	}
+
+	fmt.Println("executing UpdateNetworkAccessPolicySetByID...")
+	policy_setByID, _, err := Client.NetworkAccessPolicySet.UpdateNetworkAccessPolicySetByID("13727e31-93d1-4fc6-9a7e-80f6998b1916", policy_set4)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println("executing GetAuthorizationProfileByName...")
-	profByID, _, err := Client.AuthorizationProfile.GetAuthorizationProfileByName("Internet_Only")
-	if err != nil {
-		fmt.Println(err)
+	if policy_setByID != nil {
+		fmt.Println(policy_setByID.Response.Condition)
 	}
-	if profByID != nil && profByID.AuthorizationProfile != nil {
-		newProfileID := profByID.AuthorizationProfile.ID
-		fmt.Println("-------------")
-		fmt.Println("ID: ", profByID.AuthorizationProfile.ID)
-		fmt.Println("Name: ", profByID.AuthorizationProfile.Name)
-		fmt.Println("Description: ", profByID.AuthorizationProfile.Description)
-		fmt.Println("AdvancedAttributes: ", profByID.AuthorizationProfile.AdvancedAttributes)
-		fmt.Println("AccessType: ", profByID.AuthorizationProfile.AccessType)
-		fmt.Println("AuthzProfileType: ", profByID.AuthorizationProfile.AuthzProfileType)
-		fmt.Println("VLAN: ", profByID.AuthorizationProfile.VLAN)
-		fmt.Println("Reauth: ", profByID.AuthorizationProfile.Reauth)
-		fmt.Println("WebRedirection: ", profByID.AuthorizationProfile.WebRedirection)
-		fmt.Println("TrackMovement: ", profByID.AuthorizationProfile.TrackMovement)
-		fmt.Println("AgentlessPosture: ", profByID.AuthorizationProfile.AgentlessPosture)
-		fmt.Println("ServiceTemplate: ", profByID.AuthorizationProfile.ServiceTemplate)
-		fmt.Println("EasywiredSessionCandidate: ", profByID.AuthorizationProfile.EasywiredSessionCandidate)
-		fmt.Println("DaclName: ", profByID.AuthorizationProfile.DaclName)
-		fmt.Println("VoiceDomainPermission: ", profByID.AuthorizationProfile.VoiceDomainPermission)
-		fmt.Println("Neat: ", profByID.AuthorizationProfile.Neat)
-		fmt.Println("WebAuth: ", profByID.AuthorizationProfile.WebAuth)
-		fmt.Println("ProfileName: ", profByID.AuthorizationProfile.ProfileName)
-		fmt.Println("Link: ", profByID.AuthorizationProfile.Link)
-		fmt.Println("-------------")
 
-		fmt.Println("executing DeleteAuthorizationProfileByID...")
-		_, err = Client.AuthorizationProfile.DeleteAuthorizationProfileByID(newProfileID)
+	//if profByID != nil && profByID.AuthorizationProfile != nil {
+	//	newProfileID := profByID.AuthorizationProfile.ID
+	//	fmt.Println("-------------")
+	//	fmt.Println("ID: ", profByID.AuthorizationProfile.ID)
+	//	fmt.Println("Name: ", profByID.AuthorizationProfile.Name)
+	//	fmt.Println("Description: ", profByID.AuthorizationProfile.Description)
+	//	fmt.Println("AdvancedAttributes: ", profByID.AuthorizationProfile.AdvancedAttributes)
+	//	fmt.Println("AccessType: ", profByID.AuthorizationProfile.AccessType)
+	//	fmt.Println("AuthzProfileType: ", profByID.AuthorizationProfile.AuthzProfileType)
+	//	fmt.Println("VLAN: ", profByID.AuthorizationProfile.VLAN)
+	//	fmt.Println("Reauth: ", profByID.AuthorizationProfile.Reauth)
+	//	fmt.Println("WebRedirection: ", profByID.AuthorizationProfile.WebRedirection)
+	//	fmt.Println("TrackMovement: ", profByID.AuthorizationProfile.TrackMovement)
+	//	fmt.Println("AgentlessPosture: ", profByID.AuthorizationProfile.AgentlessPosture)
+	//	fmt.Println("ServiceTemplate: ", profByID.AuthorizationProfile.ServiceTemplate)
+	//	fmt.Println("EasywiredSessionCandidate: ", profByID.AuthorizationProfile.EasywiredSessionCandidate)
+	//	fmt.Println("DaclName: ", profByID.AuthorizationProfile.DaclName)
+	//	fmt.Println("VoiceDomainPermission: ", profByID.AuthorizationProfile.VoiceDomainPermission)
+	//	fmt.Println("Neat: ", profByID.AuthorizationProfile.Neat)
+	//	fmt.Println("WebAuth: ", profByID.AuthorizationProfile.WebAuth)
+	//	fmt.Println("ProfileName: ", profByID.AuthorizationProfile.ProfileName)
+	//	fmt.Println("Link: ", profByID.AuthorizationProfile.Link)
+	//	fmt.Println("-------------")
 
-		if err != nil {
-			fmt.Println(err)
-		}
-	}
+	//	fmt.Println("executing DeleteAuthorizationProfileByID...")
+	//	_, err = Client.AuthorizationProfile.DeleteAuthorizationProfileByID(newProfileID)
+
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
+	//}
 }
